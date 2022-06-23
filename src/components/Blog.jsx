ae18452ebc1a7821c7fa985fb073db05f2f24bac
mehedih23 <nmehedi23@gmail.com>
2022-06-23 14:30:04 +0600

    blog responsive done

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -118,6 +118,15 @@ position: relative;
 }
 
 
+
+@media screen and (min-width: 280px) and (max-width: 1080px){
+    margin: 2rem 0;
+    .blogs{
+        padding: 0 2rem;
+        grid-template-columns: 1fr;
+        margin: 0 1rem;
+    }       
+}
 `
 
 export default Blog

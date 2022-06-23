71ab4992b587ae326edb76d32cb3fbaedba87dd7
mehedih23 <nmehedi23@gmail.com>
2022-06-23 15:08:40 +0600

    skills responsive done

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -163,6 +163,52 @@ background-color: var(--secondary-color);
     }
 }
 
+
+@media screen and (min-width: 280px) and (max-width: 1080px){
+    overflow-x: hidden;
+    padding: 2rem 0;
+    .background{
+        display: none;
+    }
+    .skills_title{
+        padding: 2rem;
+        text-align: center;
+        h2{
+            font-size: 1.5rem;
+        }
+    }
+    .skills{
+        padding: 0;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: 1rem;
+        &_bars{
+            padding: 0.2rem;
+            gap: 0rem;
+            align-items: center;
+            justify-content: center;
+            &_bar{
+                .container{
+                    gap: 1rem;
+                    progress{
+                        width: 12rem;
+                        height: 0.5rem;
+                        &::-webkit-progress-bar{
+                            height: 0.3rem;
+                        }
+                    }
+                }
+                h3{
+                    font-size: 1rem;
+                }
+            }
+        }
+        &_content{
+            padding: 0 2rem;
+        }
+    }
+}
 `
 
 export default Skills

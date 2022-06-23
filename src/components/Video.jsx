db5629d6d61b06cc3321f19e91fe11becb531136
mehedih23 <nmehedi23@gmail.com>
2022-06-23 14:32:54 +0600

    video responsive done

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -41,6 +41,17 @@ position: relative;
     z-index: 10;
 }
 
+
+@media screen and (min-width: 280px) and (max-width: 1080px){
+  height: 100%;
+  padding: 6rem 2rem;
+  .video{
+    iframe{
+      width: 80vw;
+      height: 10rem;
+    }
+  }
+}
 `;
 
 export default Video
